550e69aef258d9a23c73f3e1e5b0c4d115248db9
Suresh Patel <sureshpatel99@iitbombay.org>
2013-07-12 02:18:59 +0000

    First cut of readFileSync and Buffer.toString

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
 var express = require('express');
+var infile = "index.html";
+
+buf = new Buffer(256);
+
+var str1 = buf.toString(fs.readFileSync(infile));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str1);
 });
 
 var port = process.env.PORT || 5000;
