dc558bc2a34547f4469855ecafe551239deadd3e
Suresh Patel <sureshpatel99@iitbombay.org>
2013-07-12 07:27:47 +0000

    Used the filename directly rather than using a variable

--- web.js
+++ web.js
@@ -1,10 +1,7 @@
-var express = require('express');
-
 var fs = require('fs');
-var infile = "index.html";
+var str1 = fs.readFileSync('index.html', {encoding: 'utf8'});
 
-var str1 = fs.readFileSync(infile, {encoding: 'utf8'});
-
+var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
